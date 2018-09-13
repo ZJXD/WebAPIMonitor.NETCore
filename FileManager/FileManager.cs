cf6f4878e38b44c94e3f96ff3146d7d2b8c24115
ZHT <MI-DIT@DESKTOP-ALVUP6K>
2018-09-13 17:54:04 +0800

    IFileProvider 使用
    .NET Core IFileProvider 的基本使用（对于上次上传不完全）

---
+++ FileManager/FileManager.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    public class FileManager : IFileManager
+    {
+        public IFileProvider FileProvider { get; set; }
+
+        public FileManager(IFileProvider fileProvider)
+        {
+            this.FileProvider = fileProvider;
+        }
+
+        private void Render(string subPath, ref int layer, Action<int, string> render)
+        {
+            layer++;
+
+            foreach (var fileInfo in this.FileProvider.GetDirectoryContents(subPath))
+            {
+                render(layer, fileInfo.Name);
+                if (fileInfo.IsDirectory)
+                {
+                    Render($@"{subPath}\{fileInfo.Name}".TrimStart('\\'), ref layer, render);
+                }
+            }
+
+            layer--;
+        }
+
+        public void ShowStructure(Action<int, string> render)
+        {
+            int layer = -1;
+            Render("", ref layer, render);
+        }
+
+        public async Task<string> ReadAllTextAsync(string path)
+        {
+            byte[] buffer;
+            using (Stream readStream = this.FileProvider.GetFileInfo(path).CreateReadStream())
+            {
+                buffer = new byte[readStream.Length];
+                await readStream.ReadAsync(buffer, 0, buffer.Length);
+            }
+            return Encoding.ASCII.GetString(buffer);
+        }
+    }
+}
