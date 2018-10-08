485c202a8f529fd411c33193c6d5ae96e2758c12
ZJXD <zhuhtd@foxmail.com>
2018-10-08 17:35:35 +0800

    Razor 示例功能完善
    SQL Server LocalDB
    更新页面
    添加搜索
    添加新字段
    添加验证等功能的实现

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +9,31 @@ namespace RazorPagesMovie.Models
 {
     public class Movie
     {
+        // 各个属性上面的特性是对数据进行验证，基于 DataAnnotations
+
         public int ID { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
 
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Required]
+        [StringLength(5)]
+        public string Rating { get; set; }
     }
 }
--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -18,11 +19,37 @@ namespace RazorPagesMovie.Pages.Movies
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; }
+        public IList<Movie> Movie { get; set; }
+        public SelectList Genres { get; set; }     // 电影流派列表
+        public string MovieGenre { get; set; }     // 查询的电影流派
 
-        public async Task OnGetAsync()
+        /// <summary>
+        /// 可搜索接口
+        /// </summary>
+        /// <param name="searchString">名称搜索条件</param>
+        /// <param name="movieGenre">电影流派</param>
+        /// <returns></returns>
+        public async Task OnGetAsync(string searchString, string movieGenre)
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(m => m.Title.Contains(searchString));
+            }
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(m => m.Genre == movieGenre);
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+
+            Movie = await movies.ToListAsync();
         }
     }
 }
